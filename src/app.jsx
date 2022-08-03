02833c81bb2f0d1c6de0033d1d5f1ed44d2928fd
Tamoor Islam <tamoorislam076@gmail.com>
2022-08-03 15:04:57 +0500

    Add imports

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-// import LandingPage from "./pages/landingPage";
+import LandingPage from "./pages/landingPage";
 import LoginPage from "./pages/authentication/login";
-// import HomePage from "./pages/homePage";
+import HomePage from "./pages/homePage";
 function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<LandingPage />} /> */}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/homePage" element={<HomePage />} /> */}
+        <Route path="/homePage" element={<HomePage />} />
       </Routes>
     </>
   );
