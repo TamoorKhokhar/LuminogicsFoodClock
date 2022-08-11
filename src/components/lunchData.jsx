b575c6869ea72246089e28347f61ddf542eda1a1
Tamoor Islam <tamoorislam076@gmail.com>
2022-08-10 23:36:10 -0700

    Code changings in Lunch and tea data

--- src/components/lunchData.jsx
+++ src/components/lunchData.jsx
@@ -1,57 +1,88 @@
-import React from "react";
-import { Grid, Typography, Box } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import { ThemeProvider } from "@emotion/react";
-import theme from "../theme/theme";
-import { useSelector } from "react-redux";
-import Buttons from "./buttonContainer";
+import React from 'react';
+import { Grid, Typography, Box } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { ThemeProvider } from '@emotion/react';
+import theme from '../theme/theme';
+import { useState } from 'react';
+import Buttons from './buttonContainer';
 function LunchData() {
-  const user = useSelector((state) => {
-    return state?.signIn?.signIn[0]?.payload?.data?.user?.userName;
-  });
+  const [userName, setUserName] = useState('');
+  const [items, setItems] = useState('');
+  const [rotti, setRotti] = useState('');
+  const [amountPaid, setAmountPaid] = useState('');
+  const SubmitForm = (e) => {
+    e.preventDefault();
+  };
   return (
     <ThemeProvider theme={theme}>
       <Grid
         container
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          minHeight: "max-content"
+          display: 'flex',
+          justifyContent: 'center',
+          minHeight: 'max-content'
         }}>
-        <Grid
-          item
-          md={12}
-          xs={12}
+        <Box
           component="form"
+          onSubmit={SubmitForm}
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignitem: "center",
-            marginTop: "1rem",
-            marginBottom: "1rem",
-            width: "100%",
-            borderRadius: "5px",
-            height: "max-content"
+            display: 'flex',
+            justifyContent: 'center',
+            alignitem: 'center',
+            marginTop: '1rem',
+            marginBottom: '1rem',
+            width: '100%',
+            borderRadius: '5px',
+            height: 'max-content'
           }}>
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              width: "90%",
-              color: "#132F4C"
+              display: 'flex',
+              flexDirection: 'column',
+              width: '90%',
+              color: '#132F4C'
             }}>
             <Typography variant="h4">Lunch Details</Typography>
             <Typography variant="p1">User Name:</Typography>
-            <TextField required id="outlined-required" label="Required" value={user} />
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
             <Typography variant="p1">Item Description:</Typography>
-            <TextField required id="outlined-required" label="Required" />
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              value={items}
+              onChange={(e) => setItems(e.target.value)}
+            />
             <Typography variant="p1">Rotti:</Typography>
-            <TextField required id="outlined-required" label="Required" type="number" />
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              type="number"
+              value={rotti}
+              onChange={(e) => setRotti(e.target.value)}
+            />
             <Typography variant="p1">Amount Paid:</Typography>
-            <TextField required id="outlined-required" label="Required" type="number" />
-            <Buttons />
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              type="number"
+              value={amountPaid}
+              onChange={(e) => setAmountPaid(e.target.value)}
+            />
+            <Buttons
+              onClick={SubmitForm}
+              disabled={userName === '' || items === '' || rotti === '' || amountPaid === ''}
+            />
           </Box>
-        </Grid>
+        </Box>
       </Grid>
     </ThemeProvider>
   );
--- src/components/teaData.jsx
+++ src/components/teaData.jsx
@@ -1,47 +1,77 @@
-import React from "react";
-import { Grid, Typography, Box } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import { ThemeProvider } from "@emotion/react";
-import DropDown from "../components/dropDown";
-import theme from "../theme/theme";
-import Buttons from "./buttonContainer";
+import React from 'react';
+import { Grid, Typography, Box } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { ThemeProvider } from '@emotion/react';
+import DropDown from '../components/dropDown';
+import theme from '../theme/theme';
+import Buttons from './buttonContainer';
+import { useState } from 'react';
 function TeaData() {
+  const [userName, setUserName] = useState('');
+  const [sugarQuantity, setSugarQuantity] = useState('');
+  const [teaVolume, setTeaVolume] = useState('');
+
+  const SubmitForm = (e) => {
+    e.preventDefault();
+  };
   return (
     <ThemeProvider theme={theme}>
       <Grid
         container
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          minHeight: "max-content"
+          display: 'flex',
+          justifyContent: 'center',
+          minHeight: 'max-content'
         }}>
         <Box
           component="form"
+          onSubmit={SubmitForm}
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignitem: "center",
-            marginTop: "1rem",
-            marginBottom: "1rem",
-            width: "100%",
-            borderRadius: "5px",
-            height: "max-content"
+            display: 'flex',
+            justifyContent: 'center',
+            alignitem: 'center',
+            marginTop: '1rem',
+            marginBottom: '1rem',
+            width: '100%',
+            borderRadius: '5px',
+            height: 'max-content'
           }}>
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              width: "90%",
-              color: "#132F4C"
+              display: 'flex',
+              flexDirection: 'column',
+              width: '90%',
+              color: '#132F4C'
             }}>
             <Typography variant="h4">Tea Details</Typography>
             <Typography variant="p1">User Name:</Typography>
-            <TextField required id="outlined-required" label="Required" />
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
             <Typography variant="p1">Sugar Quantatity:</Typography>
-            <TextField required id="outlined-required" label="Required" type="number" />
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              type="number"
+              value={sugarQuantity}
+              onChange={(e) => setSugarQuantity(e.target.value)}
+            />
             <Typography variant="p1">Tea Volume:</Typography>
-            <DropDown item1="Half Cup" item2="Full Cup" />
-            <Buttons />
+            <DropDown
+              item1="Half Cup"
+              item2="Full Cup"
+              value={teaVolume}
+              onChange={(e) => setTeaVolume(e.target.value)}
+            />
+            <Buttons
+              onClick={SubmitForm}
+              disabled={userName === '' || sugarQuantity === '' || teaVolume === ''}
+            />
           </Box>
         </Box>
       </Grid>
