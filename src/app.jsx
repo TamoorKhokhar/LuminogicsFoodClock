6f67780224059f57af9f6e0584934071d7814120
Tamoor Islam <tamoorislam076@gmail.com>
2022-08-22 11:48:34 +0500

    change path

--- src/app.jsx
+++ src/app.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<SignIn />} />
         <Route path="/signIn" element={<Protected Component={SignIn} />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/homePage" element={<Protected Component={HomePage} />} />
